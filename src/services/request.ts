f58c8bbfdfbb938224e584f3f8c993a73ce1fec5
Martin Hula <hulamart@gmail.com>
2020-05-12 17:40:40 +0200

    Add logging for outgoing requests

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,13 +1,15 @@
 import { injectable, inject } from "inversify";
 import { TYPES, Fetch } from '../types';
 import { Response } from 'node-fetch';
+import { LoggerService } from './logger';
 
 @injectable()
 export class RequestService {
 
-    constructor (@inject(TYPES.Fetch) private fetch: Fetch) {}
+    constructor (@inject(TYPES.Fetch) private fetch: Fetch, @inject(LoggerService) private loggerService: LoggerService) {}
 
     public get (url: string): Promise<Response> {
+        this.loggerService.log(`Outgoing GET request to: ${url}`);
         return this.fetch(url);
     }
 
